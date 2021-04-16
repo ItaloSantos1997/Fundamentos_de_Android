d78cc73c75615e62e36cc47cfb39c58cc39575b9
italo.santos <italo.santos@samsung.com>
2021-04-15 20:59:54 -0400

    Refatoração dos códigos

--- calculadora_de_imc/app/src/main/java/com/hefesto/calculadora_de_imc/MainActivity.kt
+++ calculadora_de_imc/app/src/main/java/com/hefesto/calculadora_de_imc/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.hefesto.calculadora_de_imc
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
@@ -8,9 +7,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.math.BigDecimal
-import kotlin.math.roundToInt
-import kotlin.math.roundToLong
-
+import java.math.RoundingMode
 
 class MainActivity : AppCompatActivity() {
     private var txt_valor1: EditText? = null
@@ -26,21 +23,16 @@ class MainActivity : AppCompatActivity() {
         txt_resultado = findViewById<View>(R.id.txt_resultado) as TextView
     }
 
-    @SuppressLint("SetTextI18n")
     fun calcular(view: View?) {
         val valor1: Double
         val valor2: Double
-        val valor: String
-        val resultado: Double
+        val valor: Double
 
         valor1 = txt_valor1!!.text.toString().toDouble()
         valor2 = txt_valor2!!.text.toString().toDouble()
 
-        resultado = valor1 / (valor2 * valor2)
-        valor = String.format("%.2f", resultado)
-        //valor = resultado.toDouble().toBigDecimal(2)
-        //resultado = valor1 / (valor2 * valor2)
-        //txt_resultado!!.text = java.lang.Double.toString(resultado)
-        txt_resultado!!.text = java.lang.Double.toString(valor)
+        valor = valor1 / (valor2 * valor2)
+        val resultado = BigDecimal(valor).setScale(2, RoundingMode.HALF_EVEN).toDouble()
+        txt_resultado!!.text = java.lang.Double.toString(resultado)
     }
 }
